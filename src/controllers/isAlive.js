36de6fbca5c72afa29b0f82f52f8b38841b688f9
nitatemic <alexandre@nitatemic.dev>
2022-02-20 21:24:05 +0100

    Remove docker

--- src/controllers/isAlive.js
+++ src/controllers/isAlive.js
@@ -1,9 +1,11 @@
+const db = require('../middlewares/dbCheck');
+
 // Fonction qui renvoie le code 200 de réponse si le serveur est en marche
 exports.isAlive = function (req, res) {
-  res
-  .status(200)
-  .send(
-    'I\'m a-stayin\' alive, stayin\' alive\n'
-    + 'Ah, ah, ah, ah, stayin\' alive, stayin\' alive\n'
-    + 'Ah, ah, ah, ah, stayin\' ali-i-i-i-ive');
+  // Vérifier que la base de données est accessible
+  if (db.isAlive()) {
+    res.status(200).send('OK');
+  } else {
+    res.status(500).send('KO');
+  }
 };
---
+++ src/middlewares/dbCheck.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json()); // To parse the incoming requests with JSON payloads
+require('dotenv').config();
+
+const mysql = require('mysql');
+
+const pool = mysql.createPool({
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+  timezone: 'Europe/Paris',
+  skipSetTimezone: true,
+});
+
+// Fonction qui check si la base de données est connectée
+exports.isAlive = () => {
+  pool.getConnection((err, connection) => {
+    if (err == null) {
+      connection.release();
+      return true;
+    }
+    return false;
+  });
+};
